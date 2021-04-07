a93c1dd808df49cac8ad15e32b667aa32685b5d8
bugbit <ohernandezba@gmail.com>
2021-04-08 00:19:40 +0200

    init

---
+++ juegecitos/Juegecitos.Blazor/wwwroot/juegecitosJsInterop.js
@@ -0,0 +1,14 @@
+﻿function Juegecitos() {
+    function loop(timeStamp)
+    {
+        window.requestAnimationFrame(loop);
+        window.juegecitos.instance.invokeMethod('Loop', timeStamp);
+    }
+    this.instance = undefined;
+    this.init = function (instance) {
+        this.instance = instance;
+        window.requestAnimationFrame(loop);
+    }
+}
+
+window.juegecitos = new Juegecitos();
