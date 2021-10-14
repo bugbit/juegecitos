6b4386f85bdf29d10345135ea082c3ed480a12c3
bubgit <ohernandezba@gmail.com>
2021-10-14 23:31:05 +0200

    jetpac

---
+++ juegecitos/Juegecitos.Blazor/Games/Jetpac/JPLevelScene.cs
@@ -0,0 +1,11 @@
+using Juegecitos.Blazor.Core;
+
+namespace Juegecitos.Blazor.Games.Jetpac
+{
+    public class JPLevelScene : Scene
+    {
+        public JPLevelScene(Game argGame) : base(argGame)
+        {
+        }
+    }
+}
--- juegecitos/Juegecitos.Blazor/Games/Jetpac/JetpacGame.cs
+++ juegecitos/Juegecitos.Blazor/Games/Jetpac/JetpacGame.cs
@@ -4,6 +4,12 @@ namespace Juegecitos.Blazor.Games.Jetpac
 {
     public class JetpacGame : Game
     {
-        
+        public override void Initialize()
+        {
+            base.Initialize();
+            PresentationParameters.Width=716;
+            PresentationParameters.Height=570;
+            PresentationParameters.MainScene=new JPLevelScene(this);
+        }
     }
 }
