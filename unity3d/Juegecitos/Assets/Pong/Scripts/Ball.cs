809d9c265a67af0685ad1a7bbf29900e965dae06
bugbit <ohernandezba@gmail.com>
2022-09-27 23:32:30 +0200

    pong

--- unity3d/Juegecitos/Assets/Pong/Scripts/Ball.cs
+++ unity3d/Juegecitos/Assets/Pong/Scripts/Ball.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    private Rigidbody2D rigid;
+    public Vector3 impulse = new Vector3(2, 0, 2);
+
+    //private Rigidbody2D rigid;
+    private Rigidbody rigid;
 
     // Start is called before the first frame update
     void Start()
     {
-        rigid = GetComponent<Rigidbody2D>();
+        //rigid = GetComponent<Rigidbody2D>();
+        rigid = GetComponent<Rigidbody>();
         //rigid.velocity = new Vector2(10, 10);
-        rigid.AddForce(new Vector2(10, 10), ForceMode2D.Impulse);
+        rigid.AddForce(impulse, ForceMode.Impulse);
     }
 
     // Update is called once per frame
