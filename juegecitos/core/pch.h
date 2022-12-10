0515db2f0ed7ac46f508d43b9887aac0ed613f1f
bugbit <ohernandezba@gmail.com>
2022-12-10 13:07:40 +0100

    update branch unity3d

--- juegecitos/core/pch.h
+++
@@ -1,8 +0,0 @@
-#include <string>
-#include <vector>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <box2d/box2d.h>
-#ifdef __EMSCRIPTEN__
-#include "emscripten.h"
-#endif
--- juegecitos/jetpac/pch.h
+++
@@ -1,7 +0,0 @@
-#include <stdlib.h>
-#include <string>
-#include <vector>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <box2d/box2d.h>
-#include "juegecitos.h"
